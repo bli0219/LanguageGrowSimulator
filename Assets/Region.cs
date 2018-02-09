e798afd508d03fcc2febaee98f4b6f76995e1586
bli-desktop <bli@trinity.edu>
2018-02-09 08:30:58 -0600

    Initial commit

---
+++ Assets/Region.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Culture {
+    European,
+    Asian
+}
+
+public enum Country: int {
+    US = 1,
+    Canada = 2
+}
+
+public enum Language {
+    English,
+    Spanish
+}
+
+
+public class Region : MonoBehaviour {
+
+    public float locationX;
+    public float locationY;
+    public float capacity;
+    public float influence;
+    public float immiBarrier;
+    public Dictionary<Language, float> first;
+    public Dictionary<Language, float> secondary;
+    public Culture culture;
+    public Country country;
+    public Language official;
+
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
