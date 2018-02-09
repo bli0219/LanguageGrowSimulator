6440bacee2e554dba5a778dd81c6c6b7ddc41856
bli-macbook <bli@trinity.edu>
2018-02-09 11:45:24 -0600

    11:45AM

--- Assets/Region.cs
+++ Assets/Region.cs
@@ -17,27 +17,101 @@ public enum Language {
     Spanish
 }
 
-
+[System.Serializable]
 public class Region : MonoBehaviour {
 
+	/*
     public float locationX;
     public float locationY;
     public float capacity;
     public float influence;
     public float immiBarrier;
-    public Dictionary<Language, float> first;
-    public Dictionary<Language, float> secondary;
     public Culture culture;
     public Country country;
     public Language official;
+	*/
+	[System.Serializable] 
+	public class LanguagePopulation
+	{
+		public Language language;
+		public float population;
+	}
 
+	[System.Serializable] 
+	public class MigrationRate
+	{
+		public Region dest;
+		public float rate;
+
+		public MigrationRate(Region dest, float rate) {
+			this.dest = dest;
+			this.rate = rate;
+		}
+	}
+
+	public LanguagePopulation[] first;
+	public LanguagePopulation[] secondary;
+	public MigrationRate[] mr; 
+	public bool paused = true;
 
     void Start () {
-		
+
+
+
+//		foreach (Region r in FindObjectsOfType<Region> ()) {
+//			if (r != this) {
+//				migrateRates[
+//			}
+//		}
+	}
+
+	public void Unpause() {
+		Region[] regions = FindObjectsOfType<Region> ();
+		mr = new MigrationRate[regions.Length-1];
+		for (int i = 0; i!=regions.Length-1; i++) {
+			mr [i] = new MigrationRate (regions [i], 0f);
+		}
+
+		first = new LanguagePopulation[2];
+		secondary = new LanguagePopulation[2];
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+	void Migrate() {
+		if (!paused) {
+//			
+			foreach (MigrationRate mrPair in mr) {
+
+//				first
+//				migrateRate.dest   migrateRate.rate * 
+			
+//				float migrateRate = migratePair.Value; // percent migrating to the dest
+//				 
+//				foreach (KeyValuePair<Language,float> speakerPair in first ) {
+//
+//					Language language = speakerPair.Key;
+//					float migrants = speakerPair.Value * migrateRate; // migrants with this language
+//
+//					first[language] -= migrants; // migrants leave
+//					dest.first[language] += migrants; // migrants arrive
+//				}
+//
+//				foreach (KeyValuePair<Language,float> speakerPair in secondary) {
+//
+//					Language language = speakerPair.Key;
+//					float migrants = speakerPair.Value * migrateRate; // migrants with this language
+//
+//					first[language] -= migrants; // migrants leave
+//					dest.first[language] += migrants; // migrants arrive
+//				}
+//
+			}
+		}
+	}
+
+
 }
--- Assets/SimulatorManager.cs
+++ Assets/SimulatorManager.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class SimulatorManager : MonoBehaviour {
 
-    GameObject regionPrefab;
+    public GameObject regionPrefab;
 
     void Start () {
 		
+	}
+
+	public void UnpauseRegions() {
+		foreach (Region r in FindObjectsOfType<Region>()) {
+			r.Unpause ();
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	    if (Input.GetMouseButtonDown(0)) {
-            Vector3 pos = Input.mousePosition;
+			Debug.Log ("pressed");
+			Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			pos.z = 0;
             GameObject region = Instantiate(regionPrefab, pos, Quaternion.identity); 
         }	
 	}
